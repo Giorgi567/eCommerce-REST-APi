66b1f3766cf4904b73d92cdaaee8330fe9fec9a2
Giorgi Gvintidze <giorgininidze200@gmail.com>
2023-07-07 22:49:10 +0400

    changes

---
+++ members_api/src/api/config/connect-mongo.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const chalk = require("chalk");
+
+const connectMongo = async () => {
+  const connected = await mongoose.connect(process.env.MONGO_URL);
+
+  console.info(
+    chalk.bold.cyan(`Mongo Connected: ${connected.connection.host}`)
+  );
+};
+
+module.exports = connectMongo;
